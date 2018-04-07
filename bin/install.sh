b08c2935c19db0e1278c89adc71ea6920feb2d85
fabifrank <info@fabifrank.com>
2018-04-07 22:08:51 +0200

    added deploy script; fix install exception handling

--- bin/install.sh
+++ bin/install.sh
@@ -6,9 +6,17 @@ if ! echo "$(node --version)" | grep -q "v8.9.0"; then
 fi
 
 npm install -g composer-cli@0.19.0
+if [ ! $? -eq 0 ]; then exit 1; fi
+
 npm install -g composer-rest-server@0.19.0
+if [ ! $? -eq 0 ]; then exit 1; fi
+
 npm install -g generator-hyperledger-composer@0.19.0
+if [ ! $? -eq 0 ]; then exit 1; fi
+
 npm install -g composer-playground@0.19.0
+if [ ! $? -eq 0 ]; then exit 1; fi
+
 
 filename="fabric-dev-servers.tar.gz"
 mkdir ./fabric-tools && cd ./fabric-tools
